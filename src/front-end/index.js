91b442f9c3901abd1dfa3e9c722888704a17d9d1
osmomysl <dannvx@gmail.com>
2022-11-26 21:06:30 +0200

    pass client api to front end

--- src/front-end/index.js
+++ src/front-end/index.js
@@ -1,8 +1,10 @@
 import Main from '../../../fi-front-end/src/admin.js'
+import api from '../../../fi-api/src/client/index.js'
+
 import './index.html'
 
 function main() {
-    Main(document.querySelector('#main'))
+    Main(document.querySelector('#main'), api)
 }
 
 document.addEventListener('DOMContentLoaded', main)
