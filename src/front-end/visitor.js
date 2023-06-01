62204518e270c710d75aaca971ba2f9734a5f885
osmomysl <dannvx@gmail.com>
2023-06-01 23:41:19 +0300

    use fi-front-end visitor function

--- src/front-end/visitor.js
+++ src/front-end/visitor.js
@@ -1,1 +1,10 @@
+import api from '../../../fi-api/src/client/index.js'
+import Main from '../../../fi-front-end/src/visitor.js'
+
 import './visitor.html'
+
+function main() {
+    Main(api.visitor)
+}
+
+document.addEventListener('DOMContentLoaded', main)
