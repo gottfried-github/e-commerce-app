52f64f3975c98aa00147754d72fbf34495086a82
osmomysl <dannvx@gmail.com>
2023-07-05 00:41:10 +0300

    adapt new front-end name

--- src/front-end/visitor.js
+++ src/front-end/visitor.js
@@ -1,6 +1,6 @@
 import api from '../../../e-commerce-api/src/client/index.js'
-import Main from '../../../e-commerce-front-end/src/visitor.js'
-import '../../../e-commerce-front-end/src/styles/visitor.scss'
+import Main from '../../../e-commerce-react/src/visitor.js'
+import '../../../e-commerce-react/src/styles/visitor.scss'
 
 import './visitor.html'
 
