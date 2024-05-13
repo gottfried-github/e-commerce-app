20cb22bb2516dfe82911da220edd60d8ce18ac45
gottfried <gottfried.dev@gmail.com>
2024-05-14 00:10:55 +0300

    tweak initial logging

--- index.js
+++ index.js
@@ -1,81 +1,95 @@
-import path from 'path';
-import {fileURLToPath} from 'url';
+import path from "path";
+import { fileURLToPath } from "url";
 
-import session from 'express-session'
-import SessionStorage from 'connect-mongo'
+import session from "express-session";
+import SessionStorage from "connect-mongo";
 
-import express from 'express'
-import {MongoClient} from 'mongodb'
+import express from "express";
+import { MongoClient } from "mongodb";
 
-import {api as _api} from '../e-commerce-api/src/server/index.js'
-import _store from '../e-commerce-mongo/src/index.js'
+import { api as _api } from "../e-commerce-api/src/server/index.js";
+import _store from "../e-commerce-mongo/src/index.js";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 function main(port) {
-    /* ensure environment variables */
-    // database connection data
-    if (!process.env.APP_DB_NAME || !process.env.APP_DB_USER || !process.env.APP_DB_PASS || !process.env.NET_NAME) throw new Error('all of the database connection parameters environment variables must be set')
-    
-    // express-session uses this
-    if (!process.env.SESSION_SECRETS) throw new Error('SESSION_SECRETS environment variable must be set')
+  /* ensure environment variables */
+  // database connection data
+  if (
+    !process.env.APP_DB_NAME ||
+    !process.env.APP_DB_USER ||
+    !process.env.APP_DB_PASS ||
+    !process.env.NET_NAME
+  )
+    throw new Error(
+      "all of the database connection parameters environment variables must be set"
+    );
 
-    /* express-session setup (see `express-session` in e-commerce-api) */
-    const sessionOptions = {
-        secret: process.env.SESSION_SECRETS.split(' '),
-        saveUninitialized: false,
-        resave: false,
-        store: SessionStorage.create({
-            mongoUrl: `mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`,
-            collectionName: 'sessions',
-            touchAfter: 24 * 3600
-        }),
-        cookie: {
-            httpOnly: true,
-            sameSite: false,
-            secure: false,
-        }
-    }
+  // express-session uses this
+  if (!process.env.SESSION_SECRETS)
+    throw new Error("SESSION_SECRETS environment variable must be set");
 
-    if (process.env.NODE_ENV === 'production') sessionOptions.cookie.secure = true
+  /* express-session setup (see `express-session` in e-commerce-api) */
+  const sessionOptions = {
+    secret: process.env.SESSION_SECRETS.split(" "),
+    saveUninitialized: false,
+    resave: false,
+    store: SessionStorage.create({
+      mongoUrl: `mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`,
+      collectionName: "sessions",
+      touchAfter: 24 * 3600,
+    }),
+    cookie: {
+      httpOnly: true,
+      sameSite: false,
+      secure: false,
+    },
+  };
 
-    /* connect to database */
-    const client = new MongoClient(`mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`)
-    client.connect()
+  if (process.env.NODE_ENV === "production")
+    sessionOptions.cookie.secure = true;
 
-    /* initialize store and api */
-    const store = _store(client.db(process.env.APP_DB_NAME), client)
-    const api = _api(store, {
-        productUploadPath: 'public/product', 
-        productDiffPath: 'public',
-        root: __dirname,
-        productPublicPrefix: '/'
-    })
+  /* connect to database */
+  const client = new MongoClient(
+    `mongodb://${process.env.APP_DB_USER}:${process.env.APP_DB_PASS}@${process.env.NET_NAME}/${process.env.APP_DB_NAME}`
+  );
+  client.connect();
 
-    /* express application setup */
-    const app = express()
+  /* initialize store and api */
+  const store = _store(client.db(process.env.APP_DB_NAME), client);
+  const api = _api(store, {
+    productUploadPath: "public/product",
+    productDiffPath: "public",
+    root: __dirname,
+    productPublicPrefix: "/",
+  });
 
-    app.use(session(sessionOptions))
+  /* express application setup */
+  const app = express();
 
-    app.use('/api/', api)
-    
-    // send index.html from anywhere: let front-end handle routing
-    app.use(["/admin", "/admin/*"], (req, res, next) => {
-        res.sendFile(path.join(__dirname, './dist/front-end/admin.html'))
-    })
+  app.use(session(sessionOptions));
 
-    app.use('/', express.static(path.join(__dirname, './dist/front-end')))
-    app.use('/', express.static(path.join(__dirname, './public')))
+  app.use("/api/", api);
 
-    app.use('/*', (req, res, next) => {
-        res.sendFile(path.join(__dirname, './dist/front-end/visitor.html'))
-    })
+  // send index.html from anywhere: let front-end handle routing
+  app.use(["/admin", "/admin/*"], (req, res, next) => {
+    res.sendFile(path.join(__dirname, "./dist/front-end/admin.html"));
+  });
 
-    /* start server */
-    const server = app.listen(3000)
+  app.use("/", express.static(path.join(__dirname, "./dist/front-end")));
+  app.use("/", express.static(path.join(__dirname, "./public")));
 
-    return {app, server, api, store}
+  app.use("/*", (req, res, next) => {
+    res.sendFile(path.join(__dirname, "./dist/front-end/visitor.html"));
+  });
+
+  /* start server */
+  const server = app.listen(3000, () => {
+    console.log("listening on port 3000");
+  });
+
+  return { app, server, api, store };
 }
 
-console.log(main(3000))
-console.log(import.meta.url)
+main(3000);
+console.log(import.meta.url);
